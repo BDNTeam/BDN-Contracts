209496f96f11797465ee6cf264be7edd22d74776
Joe Chu <aidai524@gmail.com>
2018-06-09 15:20:29 +0800

    Update ERC20Interface.sol

--- contracts/token/ERC20Interface.sol
+++ contracts/token/ERC20Interface.sol
@@ -4,7 +4,7 @@ contract ERC20Interface {
 
     string public constant name = "BDN Token";
     string public constant symbol = "BDN";
-    uint8 public constant decimals = 18;  // 18 is the most common number of decimal places
+    uint8 public constant decimals = 8;  // 8 is the most common number of decimal places
     // 0.0000000000000000001  个代币
 
     function totalSupply() public constant returns (uint);
